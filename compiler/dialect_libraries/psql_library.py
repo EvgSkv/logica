f78a8b46f20b4cca59dc2cb8d7d353b55b719d34
Evgeny Skvortsov <EvgSkv@users.noreply.github.com>
2021-04-24 15:43:53 -0700

    Making ArgMin work in psql.

--- compiler/dialect_libraries/psql_library.py
+++ compiler/dialect_libraries/psql_library.py
@@ -1,6 +1,6 @@
 library = """
-->(left:, right:) =
-  SqlExpr("STRUCT({left} as arg, {right} as value)", {left:, right:});
+->(left:, right:) = {arg: left, value: right};
+
 ArgMin(a) = SqlExpr("(ARRAY_AGG({arg} order by {value}))[1]",
                     {arg: a.arg, value: a.value})
 """
