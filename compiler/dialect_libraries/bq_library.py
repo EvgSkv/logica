a82ca1379e6567109dcc0c7d2374f2689219c03e
Evgeny Skvortsov <EvgSkv@users.noreply.github.com>
2021-04-24 23:07:05 -0700

    Supporting Arg<X> in psql.

--- compiler/dialect_libraries/bq_library.py
+++ compiler/dialect_libraries/bq_library.py
@@ -1,23 +1,25 @@
 library = """
 ->(left:, right:) = {arg: left, value: right};
 
-ArgMin(a) = SqlExpr("ARRAY_AGG({arg} order by {value} limit 1)[OFFSET(0)]",
+# All ORDER BY arguments are wrapped, to avoid confusion with
+# column index.
+ArgMin(a) = SqlExpr("ARRAY_AGG({arg} order by [{value}][offset(0)] limit 1)[OFFSET(0)]",
                     {arg: a.arg, value: a.value});
 
 ArgMax(a) = SqlExpr(
-  "ARRAY_AGG({arg} order by {value} desc limit 1)[OFFSET(0)]",
+  "ARRAY_AGG({arg} order by  [{value}][offset(0)] desc limit 1)[OFFSET(0)]",
   {arg: a.arg, value: a.value});
 
 ArgMaxK(a, l) = SqlExpr(
-  "ARRAY_AGG({arg} order by {value} desc limit {lim})",
+  "ARRAY_AGG({arg} order by  [{value}][offset(0)] desc limit {lim})",
   {arg: a.arg, value: a.value, lim: l});
 
 ArgMinK(a, l) = SqlExpr(
-  "ARRAY_AGG({arg} order by {value} limit {lim})",
+  "ARRAY_AGG({arg} order by  [{value}][offset(0)] limit {lim})",
   {arg: a.arg, value: a.value, lim: l});
 
 Array(a) = SqlExpr(
-  "ARRAY_AGG({value} order by {arg})",
+  "ARRAY_AGG({value} order by [{arg}][offset(0)])",
   {arg: a.arg, value: a.value});
 
 """
