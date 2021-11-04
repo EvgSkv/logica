ca8cb51cadb45dfc83b7d9310e2b47d10fae3f47
EvgSkv <EvgSkv@users.noreply.github.com>
2021-11-03 21:06:13 -0700

    Fix a bug of Range(0) in SQLite dialect.

--- compiler/dialects.py
+++ compiler/dialects.py
@@ -84,7 +84,7 @@ class SqLiteDialect(Dialect):
         'Range': ('(select json_group_array(n) from (with recursive t as'
                   '(select 0 as n union all '
                   'select n + 1 as n from t where n + 1 < {0}) '
-                  'select n from t))'),
+                  'select n from t) where n < {0})'),
         'ValueOfUnnested': '{0}.value',
         'List': 'JSON_GROUP_ARRAY({0})',
         'Size': 'JSON_ARRAY_LENGTH({0})',
