8a9a75fb1f34a60b08c564b68f5c9d624c77d825
Evgeny Skvortsov <71790359+EvgSkv@users.noreply.github.com>
2023-01-09 10:45:57 -0800

    Adding an ability to call OpenAI from SQLite engine.

--- common/intelligence.py
+++ common/intelligence.py
@@ -53,7 +53,7 @@ def Intelligence(command):
     model="text-davinci-003",
     prompt = command,
     temperature=0.7,
-    max_tokens=512,
+    max_tokens=1024,
     top_p=1,
     frequency_penalty=0,
     presence_penalty=0
