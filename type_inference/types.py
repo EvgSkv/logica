4d4b683a9cf5e6a4bea14cea0ee0093275a014cb
Evgeny Khoroshavin <themplarer@gmail.com>
2023-03-16 10:15:10 +0500

    added variable_types

--- type_inference/types.py
+++ type_inference/types.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 class TypesGraph:
   def __init__(self):
     self.variable_connections = defaultdict(lambda: defaultdict(lambda: []))
+    self.variable_types = dict()
 
   def connect(self, first_variable: str, second_variable: str, context):
     self.variable_connections[first_variable][second_variable].append(context)
