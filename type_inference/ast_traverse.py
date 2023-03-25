f02a35fe74bf369bf1f5d5f18b674b19345cab2d
Evgeny Khoroshavin <themplarer@gmail.com>
2023-03-25 20:08:40 +0500

    fix

--- type_inference/ast_traverse.py
+++ type_inference/ast_traverse.py
@@ -7,6 +7,9 @@ from type_inference.types import TypesGraph
 def traverse_tree(rule):
   types_graph = TypesGraph()
 
+  for field in rule["head"]["record"]["field_value"]:
+    pass
+
   for conjunct in rule["body"]["conjunction"]["conjunct"]:
     # todo: fill types_graph by matching conject type - separate task
     pass
@@ -16,10 +19,12 @@ def traverse_tree(rule):
 
 def run(raw_program: str):
   parsed = parse.ParseFile(raw_program)
+  graphs = dict()
 
   for rule in parsed["rule"]:
-    # todo: use this result - separate task
-    types_graph = traverse_tree(rule)
+    graphs[rule["head"]["predicate_name"]] = traverse_tree(rule)
+
+  # todo: use this result - separate task
 
 
 if __name__ == '__main__':
