1c5403dac2e13a07471124049e4fc80a679b9c46
Kseniia Astakhova <astakhova.ksyusha.07@mail.ru>
2023-06-12 12:21:54 +0500

    add tests for psql

--- type_inference/tests/db_interaction_test.py
+++ type_inference/tests/db_interaction_test.py
@@ -16,8 +16,11 @@
 
 import sqlite3
 import unittest
-from typing import Dict
+from typing import Dict, Tuple, List
 
+from sqlalchemy import MetaData, Table, Column, Integer, create_engine
+
+from type_inference.inspectors.postgres_inspector import PostgresInspector
 from type_inference.inspectors.sqlite_inspector import SQLiteInspector
 from type_inference.type_inference_service import TypeInference
 from type_inference.types.edge import Equality
@@ -28,22 +31,22 @@ from type_inference.types.variable_types import NumberType
 number = NumberType()
 
 
-def create_table(table_name: str, columns: Dict[str, str]):
-  with sqlite3.connect('logica.db') as conn:
-    columns_to_create = ', '.join([f'{column[0]} {column[1]}' for column in columns.items()])
-    cursor = conn.cursor()
-    cursor.execute(f'create table if not exists {table_name} (id integer, {columns_to_create})').fetchall()
+class TestTypeInferenceWithSqlite(unittest.TestCase):
+  @staticmethod
+  def create_table(table_name: str, columns: Dict[str, str]):
+    with sqlite3.connect('logica.db') as conn:
+      columns_to_create = ', '.join([f'{column[0]} {column[1]}' for column in columns.items()])
+      cursor = conn.cursor()
+      cursor.execute(f'create table if not exists {table_name} (id integer, {columns_to_create})').fetchall()
 
+  @staticmethod
+  def safe_drop_table(table_name: str):
+    with sqlite3.connect('logica.db') as conn:
+      conn.cursor().execute(f'drop table if exists {table_name}').fetchall()
 
-def safe_drop_table(table_name: str):
-  with sqlite3.connect('logica.db') as conn:
-    conn.cursor().execute(f'drop table if exists {table_name}').fetchall()
-
-
-class TestTypeInferenceWithDb(unittest.TestCase):
   def test_when_linked_with_predicate_from_db(self):
     # 'Q(x) :- T(x)'
-    create_table('T', {'col0': 'integer'})
+    self.create_table('T', {'col0': 'integer'})
     graph = TypesGraph()
     q_col0 = Variable('col0')
     t_col0 = PredicateAddressing('T', 'col0')
@@ -59,7 +62,7 @@ class TestTypeInferenceWithDb(unittest.TestCase):
     self.assertEquals(q_col0.type, number)
     self.assertEquals(t_col0.type, number)
 
-    safe_drop_table('T')
+    self.safe_drop_table('T')
 
   def test_when_linked_with_unknown_predicate(self):
     # 'Q(x) :- T(x)'
@@ -76,4 +79,51 @@ class TestTypeInferenceWithDb(unittest.TestCase):
     with self.assertRaises(KeyError):
       TypeInference(graphs, sqlite_inspector).Infer()
 
-    safe_drop_table('T')
+
+class TestTypeInferenceWithPsql(unittest.TestCase):
+  @staticmethod
+  def create_table(table_name: str, columns: List[Column]) -> Tuple[MetaData, Table]:
+    metadata = MetaData()
+    table = Table(table_name, metadata, *columns)
+    engine = create_engine(f'postgresql+psycopg2://logica:logica@127.0.0.1', pool_recycle=3600)
+    metadata.create_all(engine)
+    return metadata, table
+
+  @staticmethod
+  def safe_drop_table(metadata: MetaData, table: Table):
+    metadata.remove(table)
+
+  def test_when_linked_with_predicate_from_db(self):
+    # 'Q(x) :- T(x)'
+    metadata, table = self.create_table('T', [Column('col0', Integer, primary_key=True)])
+    graph = TypesGraph()
+    q_col0 = Variable('col0')
+    t_col0 = PredicateAddressing('T', 'col0')
+    x_var = Variable('x')
+    graph.Connect(Equality(q_col0, x_var, (0, 0)))
+    graph.Connect(Equality(x_var, t_col0, (0, 0)))
+    graphs = dict()
+    graphs['Q'] = graph
+    postgres_inspector = PostgresInspector('logica', 'logica', None)
+
+    TypeInference(graphs, postgres_inspector).Infer()
+
+    self.assertEquals(q_col0.type, number)
+    self.assertEquals(t_col0.type, number)
+
+    self.safe_drop_table(metadata, table)
+
+  def test_when_linked_with_unknown_predicate_psql(self):
+    # 'Q(x) :- T(x)'
+    graph = TypesGraph()
+    q_col0 = Variable('col0')
+    t_col0 = PredicateAddressing('T', 'col0')
+    x_var = Variable('x')
+    graph.Connect(Equality(q_col0, x_var, (0, 0)))
+    graph.Connect(Equality(x_var, t_col0, (0, 0)))
+    graphs = dict()
+    graphs['Q'] = graph
+    sqlite_inspector = SQLiteInspector('../tests/logica.db', None)
+
+    with self.assertRaises(KeyError):
+      TypeInference(graphs, sqlite_inspector).Infer()
