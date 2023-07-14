2bf96fe55a90000d73b1a0a2870c3733c4c2678a
Evgeny Skvortsov <71790359+EvgSkv@users.noreply.github.com>
2023-07-14 14:24:55 -0700

    Type inference: enable psql records.

--- type_inference/research/infer.py
+++ type_inference/research/infer.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import hashlib
 import sys
 
 if '.' not in __package__:
@@ -97,7 +98,9 @@ def ExpressionsIterator(node):
   for f in ExpressionFields():
     if f in node:
       yield node[f]
+  
   if 'record' in node and 'field_value' not in node['record']:
+  # if 'record' in node and 'expression_heritage' in node:
     yield node['record']
   if 'the_list' in node:
     for e in node['the_list']['element']:
@@ -127,6 +130,9 @@ def ActMindingPodLiterals(node):
       if 'the_string' in e['literal']:
         reference_algebra.Unify(e['type']['the_type'], reference_algebra.TypeReference('Str'))
 
+def ActClearingTypes(node):
+  if 'type' in node:
+    del node['type']
 
 class TypesInferenceEngine:
   def __init__(self, parsed_rules):
@@ -136,6 +142,12 @@ class TypesInferenceEngine:
     self.complexities = BuildComplexities(self.dependencies)
     self.parsed_rules = list(sorted(self.parsed_rules, key=lambda x: self.complexities[x['head']['predicate_name']]))
     self.types_of_builtins = types_of_builtins.TypesOfBultins()
+    self.typing_preamble = None
+
+  def CollectTypes(self):
+    collector = TypeCollector(self.parsed_rules)
+    collector.CollectTypes()
+    self.typing_preamble = collector.typing_preamble
 
   def UpdateTypes(self, rule):
     predicate_name = rule['head']['predicate_name']
@@ -163,23 +175,28 @@ class TypesInferenceEngine:
 
   def InferTypes(self):
     for rule in self.parsed_rules:
+      if rule['head']['predicate_name'][0] == '@':
+        continue
       t = TypeInferenceForRule(rule, self.types_of_builtins)
       t.PerformInference()
       self.UpdateTypes(rule)
 
-    def Concretize(node):
-      if isinstance(node, dict):
-        if 'type' in node:
-          node['type']['the_type'] = reference_algebra.VeryConcreteType(
-            node['type']['the_type'])
     for rule in self.parsed_rules:
-      Walk(rule, Concretize)
-    
+      Walk(rule, ConcretizeTypes)
+    self.CollectTypes()
+
   def ShowPredicateTypes(self):
     result_lines = []
     for predicate_name, signature in self.types_of_builtins.items():
       result_lines.append(RenderPredicateSignature(predicate_name, signature))
     return '\n'.join(result_lines)
+
+
+def ConcretizeTypes(node):
+  if isinstance(node, dict):
+    if 'type' in node:
+      node['type']['the_type'] = reference_algebra.VeryConcreteType(
+        node['type']['the_type'])
 
 
 def BuildDependencies(rules):
@@ -203,6 +220,7 @@ def BuildComplexities(dependencies):
     if p not in dependencies:
       return 0
     if p not in result:
+      result[p] = 1
       result[p] = 1 + sum(GetComplexity(x) for x in dependencies[p]) 
     return result[p]
   for p in dependencies:
@@ -297,12 +315,18 @@ class TypeInferenceForRule:
 
   def ActMindingRecordLiterals(self, node):
     if 'type' in node and 'record' in node:
+      record_type = node['type']['the_type']
+      reference_algebra.Unify(
+        record_type,
+        reference_algebra.TypeReference(
+        reference_algebra.OpenRecord()))
       for fv in node['record']['field_value']:
-        record_type = node['type']['the_type']
         field_type = fv['value']['expression']['type']['the_type']
         field_name = fv['field']
         reference_algebra.UnifyRecordField(
           record_type, field_name, field_type)
+      # print('>>>', node)
+
       node['type']['the_type'].CloseRecord()
 
   def ActMindingListLiterals(self, node):
@@ -360,6 +384,76 @@ def RenderPredicateSignature(predicate_name, signature):
   return result
 
 
+class TypeInferenceForStructure:
+  def __init__(self, structure, signatures):
+    self.structure = structure
+    self.signatures = signatures
+
+  def PerformInference(self):
+    quazy_rule = self.BuildQuazyRule()
+    Walk(quazy_rule, ActClearingTypes)
+    inferencer = TypeInferenceForRule(quazy_rule, self.signatures)
+    inferencer.PerformInference()
+          
+    Walk(quazy_rule, ConcretizeTypes)
+    collector = TypeCollector([quazy_rule])
+    collector.CollectTypes()
+
+    import json
+    # 
+    # print('>> quazy rule:', json.dumps(quazy_rule, indent=' '))
+
+  def BuildQuazyRule(self):
+    result = {}
+    result['quazy_body'] = self.BuildQuazyBody()
+    result['select'] = self.BuildSelect()
+    result['unnestings'] = self.BuildUnnestings()
+    return result
+  
+  def BuildUnnestings(self):
+    # print('>> unnestings', self.structure.unnestings)
+    result = []
+    for variable, the_list in self.structure.unnestings:
+      result.append(
+        {'inclusion': {
+          'element': variable,
+          'list': the_list
+        }}
+      )
+    return result
+
+  def BuildQuazyBody(self):
+    # print('>> tables, vars_map: ', self.structure.tables, self.structure.vars_map)
+    calls = {}
+
+    for table_id, predicate in self.structure.tables.items():
+      calls[table_id] = {
+        'predicate': {'predicate_name': predicate,
+                      'record': {'field_value': []}}}
+    
+    for (table_id, field), variable in self.structure.vars_map.items():
+      if table_id is None:
+        # This is from unnestings. I don't recall why are those even here :-(
+        # Need to clarify.
+        continue
+      calls[table_id]['predicate']['record']['field_value'].append(
+        {'field': field,
+         'value': {'expression': {'variable': {'var_name': variable}}}
+        }
+      )
+
+    return list(calls.values())
+
+  def BuildSelect(self):
+    field_values = []
+    result = {'record': {'field_value': field_values}}
+    for k, v in  self.structure.select.items():
+      field_values.append({
+        'field': k,
+        'value': {'expression': v}
+      })
+    return result
+
 class TypeErrorCaughtException(Exception):
   """Exception thrown when user-error is detected at rule-compile time."""
 
@@ -395,6 +489,7 @@ class TypeErrorChecker:
             v = node['variable']['var_name']
           else:
             v = None
+          assert 'expression_heritage' in node, node
           found_error.ReplaceIfMoreUseful(
             t, node['expression_heritage'].Display(), v,
             node)
@@ -430,3 +525,66 @@ def WalkInitializingVariables(node, get_type):
             Jog(node[k])
             type_of_variable = backed_up_types
   Jog(node)
+
+def Fingerprint(s):
+  return int(hashlib.md5(str(s).encode()).hexdigest()[:16], 16)
+
+def RecordTypeName(type_render):
+  return 'logicarecord%d' % (Fingerprint(type_render) % 1000000000)
+
+class TypeCollector:
+  def __init__(self, parsed_rules):
+    self.parsed_rules = parsed_rules
+    self.type_map = {}
+    self.psql_struct_type_name = {}
+    self.psql_type_definition = {}
+    self.definitions = []
+    self.typing_preamble = ''
+  
+  def ActPopulatingTypeMap(self, node):
+    if 'type' in node:
+      t = node['type']['the_type']
+      t_rendering = reference_algebra.RenderType(t)
+      self.type_map[t_rendering] = t
+      if isinstance(t, dict) and reference_algebra.IsFullyDefined(t):
+        node['type']['type_name'] = RecordTypeName(t_rendering)
+  
+  def CollectTypes(self):
+    Walk(self.parsed_rules, self.ActPopulatingTypeMap)
+    for t in self.type_map:
+      the_type = self.type_map[t]
+      if isinstance(the_type, dict):
+        if not reference_algebra.IsFullyDefined(the_type):
+          continue
+        self.psql_struct_type_name[t] = RecordTypeName(t)
+    self.BuildPsqlDefinitions()
+
+  def PsqlType(self, t):
+    if t == 'Str':
+      return 'text'
+    if t == 'Num':
+      return 'numeric'
+    if isinstance(t, dict):
+      return self.psql_struct_type_name[reference_algebra.RenderType(t)]
+    if isinstance(t, list):
+      [e] = t
+      return self.PsqlType(e) + '[]'
+    assert False, t
+
+  def BuildPsqlDefinitions(self):
+    for t in self.psql_struct_type_name:
+      args = ', '.join(
+        f + ' ' + self.PsqlType(v)
+        for f, v in sorted(self.type_map[t].items())
+      )
+      self.psql_type_definition[t] = f'create type %s as (%s);' % (
+        self.psql_struct_type_name[t], args)
+
+    wrap = lambda n, d: (
+      f"if not exists (select 'I(am) :- I(think)' from pg_type where typname = '{n}') then {d} end if;"
+    )
+    self.definitions = [
+      wrap(self.psql_struct_type_name[t], self.psql_type_definition[t])
+      for t in sorted(self.psql_struct_type_name, key=len)]
+
+    self.typing_preamble = 'DO $$\nBEGIN\n' + '\n'.join(self.definitions) + '\nEND $$;\n'
