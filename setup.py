62fef1e5f83bc4198bea0f061a567be06ecb17b8
skantor <serhiy.kantor@gmail.com>
2023-07-26 12:57:16 +0300

    test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='logica',
+    version='1.3.141',
+    author='PolicyEngine team',
+    author_email='',
+    url='',
+    description='logica',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
