b24a42d72143f9e4f227bb5e2261e75ccd8604ef
Kseniia Astakhova <astakhova.ksyusha.07@mail.ru>
2023-10-07 14:55:16 +0500

    second commit

--- type_inference/type_retrieval.py
+++ type_inference/type_retrieval.py
@@ -1,33 +1,45 @@
 import psycopg2
+from column import Column
+
+
+types_dict = {}
+dict = {}
+
+def unpack_type(column: Column):
+    inner_type = column.udt_name.lstrip('_')
+    cur.execute(f'''SELECT
+          pg_attribute.attname AS field_name,
+          t_0_pg_type.typname AS field_type
+        FROM
+          pg_type, pg_attribute, pg_type AS t_0_pg_type
+        WHERE
+          (pg_type.typname = \'{inner_type}\') AND
+          (pg_attribute.attrelid = pg_type.typrelid) AND
+          (t_0_pg_type.oid = pg_attribute.atttypid)''')
+    type_info = cur.fetchall()
+    if not type_info:
+        type_info = inner_type
+        if column.data_type == 'ARRAY':
+            type_info = [type_info]
+        dict[column.column_name] = type_info
+        return
+    types_dict[inner_type] = type_info
+    if column.data_type == 'ARRAY':
+        type_info = [type_info]
+    dict[column.column_name] = type_info
+
 
 conn = psycopg2.connect("dbname=logica user=logica password=logica host=127.0.0.1")
 cur = conn.cursor()
 cur.execute(f'''SELECT column_name, data_type, udt_name
                FROM information_schema.columns
                WHERE table_name = \'students\'''')
-columns_with_types = cur.fetchall()
+columns = [Column(*col) for col in cur.fetchall()]
 
-dict = {}
-
-for column_with_type in columns_with_types:
-    t = column_with_type[2].lstrip('_')
-    cur.execute(f'''SELECT
-      pg_attribute.attname AS field_name,
-      t_0_pg_type.typname AS field_type
-    FROM
-      pg_type, pg_attribute, pg_type AS t_0_pg_type
-    WHERE
-      (pg_type.typname = \'{t}\') AND
-      (pg_attribute.attrelid = pg_type.typrelid) AND
-      (t_0_pg_type.oid = pg_attribute.atttypid)''')
-    result = cur.fetchall()
-    if not result:
-        result = t
-    if column_with_type[1] == 'ARRAY':
-        result = [result]
-    dict[column_with_type[0]] = result
-    print(result)
+for column in columns:
+    unpack_type(column)
 print(dict)
+print(types_dict)
 cur.close()
 conn.close()
 
