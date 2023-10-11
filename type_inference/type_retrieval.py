aef6ce27b61353e4a7ea5355a5f82d0ad7ee48d4
Kseniia Astakhova <astakhova.ksyusha.07@mail.ru>
2023-10-11 13:26:56 +0500

    fixed and added recursion

--- type_inference/type_retrieval.py
+++ type_inference/type_retrieval.py
@@ -1,32 +1,26 @@
 import psycopg2
 from column import Column
 
+built_in = {'int4', 'varchar', 'text'}
 
-types_dict = {}
-dict = {}
-
-def unpack_type(column: Column):
-    inner_type = column.udt_name.lstrip('_')
+def unpack_type(udt_type: str) -> str:
+    if udt_type in built_in:
+        return udt_type
+    if udt_type.startswith('_'):
+        return f'[{unpack_type(udt_type.lstrip("_"))}]'
     cur.execute(f'''SELECT
           pg_attribute.attname AS field_name,
           t_0_pg_type.typname AS field_type
         FROM
           pg_type, pg_attribute, pg_type AS t_0_pg_type
         WHERE
-          (pg_type.typname = \'{inner_type}\') AND
+          (pg_type.typname = \'{udt_type}\') AND
           (pg_attribute.attrelid = pg_type.typrelid) AND
           (t_0_pg_type.oid = pg_attribute.atttypid)''')
-    type_info = cur.fetchall()
-    if not type_info:
-        type_info = inner_type
-        if column.data_type == 'ARRAY':
-            type_info = [type_info]
-        dict[column.column_name] = type_info
-        return
-    types_dict[inner_type] = type_info
-    if column.data_type == 'ARRAY':
-        type_info = [type_info]
-    dict[column.column_name] = type_info
+    type_info = []
+    for field_name, field_type in cur.fetchall():
+        type_info.append(f'{field_name}: {unpack_type(field_type)}')
+    return f'{{{", ".join(type_info)}}}'
 
 
 conn = psycopg2.connect("dbname=logica user=logica password=logica host=127.0.0.1")
@@ -37,9 +31,4 @@ cur.execute(f'''SELECT column_name, data_type, udt_name
 columns = [Column(*col) for col in cur.fetchall()]
 
 for column in columns:
-    unpack_type(column)
-print(dict)
-print(types_dict)
-cur.close()
-conn.close()
-
+    print(column.column_name, unpack_type(column.udt_name))
