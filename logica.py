5155529a38453d38fe91fd158596bc73cb5fc004
Yilin Xia <xia.yl@outlook.com>
2025-09-19 21:33:54 -0500

    reverse change

--- logica.py
+++ logica.py
@@ -40,7 +40,7 @@ import sys
 
 # We are doing this 'if' to allow usage of the code as package and as a
 # script.
-if not __package__:
+if __name__ == '__main__' and not __package__:
   from common import color
   from common import sqlite3_logica
   from compiler import functors
